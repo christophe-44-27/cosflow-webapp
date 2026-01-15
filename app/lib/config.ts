cf3a6dc95ce708e2796ff8f54f8e6b220a3ddb56
Christophe Lablancherie <clablancherie@exolnet.com>
2026-01-14 22:25:49 -0500

    update and resolve build

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: 'https://api.cosflow.co',
-  clientId: '9c73d5a2-8877-4447-9cc6-c252ab92ea8d',
-  clientSecret: '73tctXFRodzz9Dopsmh769ZuRsK7jlsrDOkHuxht',
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.cosflow.co',
+  clientId: process.env.NEXT_PUBLIC_API_CLIENT_ID || '',
+  clientSecret: process.env.NEXT_PUBLIC_API_CLIENT_SECRET || '',
 };
